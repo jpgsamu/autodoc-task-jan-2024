0d6aeb7450f5a58a13f357acdf5215fe315c6e13
jpgsamu <joao.paulo.g.samu@gmail.com>
2024-01-12 23:34:56 +0000

    create_tb_users_agg

--- task_files/sql_files/temp_del.sql
+++ task_files/sql_files/temp_del.sql
@@ -1,5 +1,16 @@
-select page_type
-,sum(session_atc_qty) as atc_sessions
-        , sum(atc_event_qty) as atc_events
-from tb_pages
-group by 1
+CREATE TABLE tb_users_agg as 
+SELECT first_sesssion_date
+     , session_qty as session_number_max
+     , CASE WHEN session_number_1st_order < 5 THEN CAST (session_number_1st_order AS STRING) 
+           ELSE '5+' END as session_number_1st_order
+
+    , SUM(1) as users
+    , SUM(CASE WHEN order_qty > 0 then 1 else 0 end) as users_with_order    
+    , SUM(session_qty) as session_qty
+    , SUM(order_qty) as order_qty
+
+
+FROM tb_users
+GROUP BY 1, 2, 3
+ORDER BY 1, 2, 3
+;
