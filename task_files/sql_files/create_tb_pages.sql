432c4ab88c0d4655b9e48bd1c804c78a2489d1ee
jpgsamu <joao.paulo.g.samu@gmail.com>
2024-01-12 22:21:16 +0000

    fix tb_pages

--- task_files/sql_files/create_tb_pages.sql
+++ task_files/sql_files/create_tb_pages.sql
@@ -5,7 +5,7 @@ SELECT session_date
      , page_type
      , COUNT(DISTINCT session) as session_qty
      , COUNT(DISTINCT CASE WHEN aux.has_event_order = 1 THEN session ELSE NULL END) as session_converted_qty
-     , COUNT(DISTINCT CASE WHEN aux.has_event_order = 1 THEN session ELSE NULL END) as session_atc_qty
+     , COUNT(DISTINCT CASE WHEN aux.has_event_add_to_cart = 1 THEN session ELSE NULL END) as session_atc_qty
      , SUM(CASE WHEN event_type = "page_view" THEN 1 ELSE 0 END) as pageview_event_qty
      , SUM(CASE WHEN event_type = "add_to_cart" THEN 1 ELSE 0 END) as atc_event_qty
      
